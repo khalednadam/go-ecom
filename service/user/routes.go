ebe63781e012e701e5f88f2b4f44082a7195f00d
Khaled Nadam <khalednadam@gmail.com>
2024-06-21 11:08:05 +0300

    feat: create user route

--- service/user/routes.go
+++ service/user/routes.go
@@ -1,9 +1,11 @@
 package user
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/khalednadam/ecomgo/service/auth"
 	"github.com/khalednadam/ecomgo/types"
 	"github.com/khalednadam/ecomgo/utils"
 )
@@ -28,5 +30,25 @@ func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
 	var payload types.RegisterUserPayload
 	if err := utils.ParseJSON(r, payload); err != nil {
 		utils.WriteError(w, http.StatusBadRequest, err)
+	}
+	_, err := h.store.GetUserByEmail(payload.Email)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("user with email %s already exists", payload.Email))
+	}
+
+	hashedPassword, err := auth.HashPassword(payload.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	err = h.store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email,
+		Password:  hashedPassword,
+	})
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
 	}
 }
